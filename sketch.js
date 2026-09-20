b374019ffdea30cc43239a2222c1a988d7387b70
James Kelleher <james.kelleher.3345@gmail.com>
2026-09-20 18:45:15 -0400

    sunday changes

--- sketch.js
+++ sketch.js
@@ -2,10 +2,18 @@ let video;
 let img;
 let handPose;
 let hands = [];
-let osc;
+let osc1, osc2, osc3;
+let filter;
+let tremolo;
+let gain;
+let lfo;
+let q = 2;
+let defaultDuration = 200;
+let duration;
 
 async function setup() {
   img = await loadImage("assets/seurat.jpg");
+  // img = await loadImage("assets/rothko.jpg");
 
   createCanvas(img.width, img.height);
 
@@ -16,9 +24,18 @@ async function setup() {
   handPose = await ml5.handPose({ flipped: true });
   handPose.detectStart(video, gotHands);
 
-  osc = new Tone.Oscillator(440, "sine").toDestination().start();
+
+  lfo = new Tone.LFO(2, 0, 1).start();
+  filter = new Tone.Filter(2000, "lowpass").toDestination();
+  gain = new Tone.Gain(1).connect(filter);
+  // gain = new Tone.Gain(1).toDestination();
+  osc1 = new Tone.Oscillator(440, "square").connect(gain);
+  osc2 = new Tone.Oscillator(440, "square").connect(gain);
+  osc3 = new Tone.Oscillator(440, "square").connect(gain);
+  lfo.connect(gain.gain);
 
   img.loadPixels();
+  duration = defaultDuration;
 }
 
 function draw() {
@@ -26,36 +43,61 @@ function draw() {
 
   let fingerX;
   let fingerY;
-  // let thumbX = 0;
-  // let thumbY = 0;
 
   image(img, 0, 0);
-  tint(255, 127);
-  image(video, 0, 0);
-  noTint();
+  // tint(255, 127);
+  // image(video, 0, 0);
+  // noTint();
 
-  if (hands.length > 0) {
-
-    fingerX = hands[0].index_finger_tip.x;
-    fingerY = hands[0].index_finger_tip.y;
-
-    // console.log(fingerX, fingerY);
-    // let thumbX = hands[0].thumb_tip.x;
-    // let thumbY = hands[0].thumb_tip.y;
+  if (hands.length == 0) {
+    osc1.stop();
+    osc2.stop();
+    osc3.stop();
+    return;
   };
 
+  if (osc1.state = 'stopped') {
+    osc1.start();
+    osc2.start();
+    osc3.start();
+  }
+
+  fingerX = hands[0].index_finger_tip.x;
+  fingerY = hands[0].index_finger_tip.y;
+  
   noStroke();
   fill(255, 255, 0);
   circle(fingerX, fingerY, 20);
   fill(255, 0, 0);
-  // circle(thumbX, thumbY, 20);
 
   fingerX = floor(fingerX);
   fingerY = floor(fingerY);
 
-  let sampleHue = samplePixels(fingerX, fingerY, 5) || 0;
-  let freq = map(sampleHue, 0, 360, 220, 2000);
-  osc.frequency.value = freq;
+  let scaleRatios = [1, 6/5, 4/3, 3/2, 9/5];
+  let fundamental = 440;
+
+  let colors = samplePixels(fingerX, fingerY, 5);
+
+  let hueSum = colors.reduce((acc, curr) => acc + hue(curr), 0);
+  let sampleHue = hueSum/colors.length || 0;
+
+  let brightSum = colors.reduce((acc, curr) => acc + brightness(curr), 0);
+  let sampleBright = brightSum / colors.length || 0;
+  
+  let resolution = 10;
+  let samp = floor(map(sampleHue, 0, 360, 0, resolution));
+
+  let ix = samp % scaleRatios.length;
+  let octavePow = floor(samp / scaleRatios.length);
+
+  let freq = fundamental * scaleRatios[ix] * pow(2, octavePow);
+  osc1.frequency.value = freq;
+  osc2.frequency.value = freq * 3/2;
+
+  // filter.Q.value = q;
+  // filter.frequency.value = lerp(map(sampleBright, 0, 100, 1000, 2000), filter.frequency.value, 0.1);
+
+  lfo.frequency.value = map(sampleBright, 0, 100, .5, 10);
 
   img.updatePixels();
 }
@@ -65,7 +107,7 @@ function gotHands(results) {
 }
 
 function samplePixels(x, y, radius) {
-  hues = [];
+  colors = [];
   for (let xx = x - radius; xx < x + radius + 1; xx++) {
     for (let yy = y - radius; yy < y + radius + 1; yy++) {
 
@@ -78,11 +120,9 @@ function samplePixels(x, y, radius) {
       pB = img.pixels[pIx + 2] || 0;
 
       let col = color(pR, pG, pB);
-      hues.push(hue(col));
+      colors.push(col);
     }
   }
 
-  let sum = hues.reduce((acc, curr) => acc + curr, 0);
-  let avg = sum/hues.length;
-  return floor(avg);
+  return colors;
 }
